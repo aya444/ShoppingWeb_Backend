3287357debbd4c055e0e0341353e894bd21f55f4
Aya Shabana <59667304+aya444@users.noreply.github.com>
2023-04-27 21:50:49 +0200

    first commit

---
+++ src/main/java/com/example/shoppingweb/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.shoppingweb;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloApplication extends Application {
+
+}
